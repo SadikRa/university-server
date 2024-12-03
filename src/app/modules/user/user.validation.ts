2036650f990f9cfe445686acc96b5c2a5c9a3e75
SadikRa <sadikrahman494@gmail.com>
2024-12-03 11:53:16 +0600

    middlewares done

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,21 +1,14 @@
-import { z } from "zod";
+import { z } from 'zod';
 
-const userSchema = z.object({
-  id: z.string().nonempty("ID is required"), // Validates that `id` is a non-empty string
-  password: z
-    .string()
-    .min(6, "Password must be at least 6 characters")
-    .max(20, "Password cannot exceed 20 characters")
-    .nonempty("Password is required"), // Validates `password` with length constraints
-  needPasswordChange: z.boolean().default(false), // Optional field with a default value
-  role: z.enum(["admin", "student", "faculty"], {
-    errorMap: () => ({ message: "Role must be 'admin', 'student', or 'faculty'" }),
-  }), // Ensures `role` is one of the specified values
-  status: z.enum(["in-progress", "blocked"], {
-    errorMap: () => ({ message: "Status must be 'in-progress' or 'blocked'" }),
-  }), // Ensures `status` is one of the specified values
-  isDeleted: z.boolean().default(false), // Optional field with a default value
+const userValidationSchema = z.object({
+  pasword: z
+    .string({
+      invalid_type_error: 'Password must be string',
+    })
+    .max(20, { message: 'Password can not be more than 20 characters' })
+    .optional(),
 });
 
-// Export the type inferred from the Zod schema (optional)
-export type UserValidation = z.infer<typeof userSchema>;
+export const UserValidation = {
+  userValidationSchema,
+};
